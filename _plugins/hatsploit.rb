19210338546ad81df522b97096d441dda88818f3
Ivan Nikolskiy <enty8080@gmail.com>
2024-09-22 01:57:00 +0100

    Update hatsploit.rb

--- _plugins/hatsploit.rb
+++ _plugins/hatsploit.rb
@@ -48,10 +48,8 @@ module Rouge
       state :hsf_prompt do
         mixin :whitespace
 
-        rule %r{exploit|auxiliary|post}, Text
-        rule %r{\:}, Punctuation
-        rule %r{\]}, Punctuation
-        rule %r{[\w/]+}, Keyword::Constant
+        rule %r{(exploit|auxiliary|post)(:)([^\]]+)(\])}, 
+          [Text, Punctuation, Tokens::Hsf::Error, Punctuation]
         rule %r{>}, Punctuation, :pop!
       end
     end
